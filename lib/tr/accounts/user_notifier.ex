9ad43617b8327e55c6639dbf7faffa65d49e0df6
kainlite <kainlite@gmail.com>
2024-03-04 16:08:54 -0300

    fix: source email and run migrations as part of the pod lifecycle

--- lib/tr/accounts/user_notifier.ex
+++ lib/tr/accounts/user_notifier.ex
@@ -8,7 +8,7 @@ defmodule Tr.Accounts.UserNotifier do
     email =
       new()
       |> to(recipient)
-      |> from({"Tr", "contact@example.com"})
+      |> from({"TechSquad", "noreply@tr.techsquad.com"})
       |> subject(subject)
       |> text_body(body)
 
