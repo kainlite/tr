5a70cf4e0e37bbc5548f636fdfc284d7cb9e2b13
kainlite <kainlite@gmail.com>
2024-03-14 01:08:16 -0300

    chore: disable dialyzer for now

--- config/dev.exs
+++ config/dev.exs
@@ -107,7 +107,7 @@ config :git_hooks,
     ],
     pre_push: [
       tasks: [
-        {:cmd, "mix dialyzer"},
+        # {:cmd, "mix dialyzer"},
         {:cmd, "mix test --color"},
         {:cmd, "echo 'success!'"}
       ]
