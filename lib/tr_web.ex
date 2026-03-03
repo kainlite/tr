5d1a7f4f5c27dc8d57650fec1ff439a3773f2c04
kainlite <kainlite@gmail.com>
2026-03-03 19:38:44 -0300

    fix: update favicon

--- lib/tr_web.ex
+++ lib/tr_web.ex
@@ -17,7 +17,9 @@ defmodule TrWeb do
   those modules here.
   """
 
-  def static_paths, do: ~w(assets fonts images assets/images favicon.ico robots.txt)
+  def static_paths,
+    do:
+      ~w(assets fonts images assets/images favicon.ico favicon.svg favicon-16x16.png favicon-32x32.png apple-touch-icon.png android-chrome-192x192.png android-chrome-512x512.png site.webmanifest robots.txt)
 
   def router do
     quote do
